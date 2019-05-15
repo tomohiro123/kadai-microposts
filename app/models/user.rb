965161f9d150de01f8cc3465ae528e6ea208c599
ogi123 <tmhrogwr123@yahoo.co.jp>
2019-05-15 11:09:53 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  has_many :favorites
+  has_many :favorite_microposts, through: :favorites, source: :micropost
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
@@ -29,5 +31,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def like(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite=favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite_micropost?(micropost)
+    self.favorite_microposts.include?(micropost)
   end
 end
